8c844f5dc6f968cb65a8f19fb2b4395f7f14aff6
Michael L Perry <michael@qedcode.com>
2022-10-15 09:31:17 -0500

    Use all properties of the parameter.

--- hotel.ts
+++ hotel.ts
@@ -1,4 +1,5 @@
 interface Reservation {
+    beds: "single" | "double" | "suite";
     name: string;
     guests: number;
 }
@@ -6,9 +7,20 @@ interface Reservation {
 function checkIn(reservation: Reservation) {
     console.log(`The party ${reservation.name} has checked in.`);
     console.log(`They have ${reservation.guests} guests.`);
+    switch (reservation.beds) {
+        case "single":
+            return "Room 101";
+        case "double":
+            return "Room 201";
+        case "suite":
+            return "Room 301";
+        default:
+            throw new Error("Unknown bed type");
+    }
 }
 
 checkIn({
+    beds: "single",
     name: "Jones Family",
     guests: 2
 });
